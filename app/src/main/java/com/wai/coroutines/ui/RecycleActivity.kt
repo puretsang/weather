f6189b6d8fef1cfebac0a878afaa4a4078b21844
weijian Tsang <puretsang@gmail.com>
2019-07-05 00:27:51 +0800

    【ADD】搭建RecyclerActivity

---
+++ app/src/main/java/com/wai/coroutines/ui/RecycleActivity.kt
@@ -0,0 +1,11 @@
+package com.wai.coroutines.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecycleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
